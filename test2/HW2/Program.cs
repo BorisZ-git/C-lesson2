0fd8f72069130e91f39f3c7152405847c33ba295
BorisZ-git <ribos@bk.ru>
2017-03-01 00:25:22 +0300

    wrote the body witout realize method

--- test2/HW2/Program.cs
+++ test2/HW2/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+/* Boris Z
+ *  2.Написать метод подсчета количества цифр числа;
+ * */
 
 namespace HW2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            //Hello(Привет)
+            Console.WriteLine("Привет! Эта программа реализует метод подсчета количества цифр числа!");
+            //Records variables(запись переменных)
+            int a = UserValue("Введите первое число: ");
+            //Show the min(показать минимальное)
+            Console.WriteLine($"Количество цифл числа: {ShowLenght(a)}");
+            //Pause
+            Console.ReadLine();
+
         }
+        static int UserValue(string msg)
+        {
+            Console.Write(msg);
+            return int.Parse(Console.ReadLine());
+        }
+        static int ShowLenght(int a)
+        {
+            int Lenght = 100;
+            return Lenght;
+        }
+
     }
 }
