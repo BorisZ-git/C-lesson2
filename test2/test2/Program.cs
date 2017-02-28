ad5d5656ff98708c7637d496d8b4b06e1141217e
BorisZ-git <ribos@bk.ru>
2017-02-28 23:46:22 +0300

    start Homework lesson2

---
+++ test2/test2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+/* Boris Z
+ * 1. Написать метод возвращающий минимальное из трех чисел
+ * */
+
+namespace test2
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
