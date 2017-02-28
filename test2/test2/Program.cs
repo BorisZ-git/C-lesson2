c67832e7e7f877a9945e11f983864749d38b7f3d
BorisZ-git <ribos@bk.ru>
2017-03-01 00:17:45 +0300

    finish correct formula for method ShowMin

--- test2/test2/Program.cs
+++ test2/test2/Program.cs
@@ -27,10 +27,12 @@ namespace test2
         }
         static int ShowMin(int a, int b, int c)
         {
-            if (a < b && a < c) return a;
-            else if (b < a && b < c) return b;
-            else if (c < a && c < b) return c;
-            else return 100;
+
+            if (a <= b && a <= c) return a;
+            else if (b <= a && b <= c) return b;
+            else return c;
+
+
         }
     }
 }
